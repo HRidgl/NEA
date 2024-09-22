561c2dc4acd5b53d9bc21cf80b40f19f49848485
HRidgl <156449683+HRidgl@users.noreply.github.com>
2024-09-22 12:55:39 +0100

    Improved commenting

--- client.py
+++ client.py
@@ -1,12 +1,14 @@
+# Importing all modules from the main
 from main import *
 
+# Class used to handle client requests and connects the client to the server
 class Client:
     
     def __init__(self):
         self.HEADER = 64  #First message to the server is 64 bytes
         self.PORT = 5050  #port location
         self.SERVER = '172.20.63.213' # fill with server ip
-        self.ADDR = (self.SERVER, self.PORT)  #makes a tupple
+        self.ADDR = (self.SERVER, self.PORT)  #makes a tuple
         self.FORMAT = 'utf-8'
         self.DISCONNECT_MESSAGE = "! DISCONNECTED"
 
@@ -15,6 +17,7 @@ class Client:
         self.client.connect(self.ADDR)
 
 
+    # Sending a message to the server
     def send(self,msg):
         message = msg.encode(self.FORMAT)
         msg_length = len(message)
@@ -27,6 +30,7 @@ class Client:
 #---------------------------------MAIN---------------------------------#
 c = Client()
 
+# Keeps asking the client for a message until they send 'STOP'
 msg = ''
 while msg != 'STOP':
   msg = input("Enter a message: ").upper()
