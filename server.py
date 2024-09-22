3a31754952b5ea5e7643bd65436bf7c6fe82614c
HRidgl <156449683+HRidgl@users.noreply.github.com>
2024-09-22 12:53:15 +0100

    Better comments

--- server.py
+++ server.py
@@ -1,18 +1,19 @@
 # Importing modules
 from main import *
 
+# Class used to make a computer turn into a server
 class Server:
 
     def __init__(self):
         self.HEADER = 64  # First message to the server is 64 bytes
         self.PORT = 5050  # port location
         self.SERVER = socket.gethostbyname(socket.gethostname())  # Gets the IPv4
-        self.ADDR = (self.SERVER, self.PORT)  # makes a tupple
+        self.ADDR = (self.SERVER, self.PORT)  # makes a tuple
         self.FORMAT = 'utf-8'
         self.DISCONNECT_MESSAGE = "! DISCONNECTED"
 
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create socket family/type
-        self.server.bind(self.ADDR)  # Binds the 2 items together in the tupple address
+        self.server.bind(self.ADDR)  # Binds the 2 items together in the tuple address
 
 
     # Handles individual connections between client and server
@@ -23,7 +24,7 @@ class Server:
         while connected:
             msg_length = conn.recv(self.HEADER).decode(self.FORMAT)  # Wait until something is sent over the socket
             if msg_length:
-                msg_length = int(msg_length)  # Shows length of the message that is about to be recieved
+                msg_length = int(msg_length)  # Shows length of the message that is about to be received
                 msg = conn.recv(msg_length).decode(self.FORMAT)
 
                 if msg == self.DISCONNECT_MESSAGE:
