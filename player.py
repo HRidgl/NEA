4ffad5fad9a1218021f64c97a7d3cfaa81f42fac
HRidgl <156449683+HRidgl@users.noreply.github.com>
2024-10-03 10:53:07 +0100

    Create player.py

---
+++ player.py
@@ -0,0 +1,9 @@
+# class for the player object to be created
+class Player:
+    
+    def __init__(self,x,y,width,height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.colour = (0,0,0)
