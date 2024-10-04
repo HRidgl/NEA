d9d4d1edcb09c8ce15e5809c9c2b1710075ea4c2
HRidgl <156449683+HRidgl@users.noreply.github.com>
2024-10-04 14:39:20 +0100

    Update client.py

--- client.py
+++ client.py
@@ -1,36 +1,48 @@
-# Importing all modules from the main
 from main import *
-from player import *
+from client import *
 
-# Class used to handle client requests and connects the client to the server
-class Client:
+class Game():
+    def __init__(self,c):
+
+        self.WIDTH = 800
+        self.HEIGHT = 600
+        self.RESOLUTION = (self.WIDTH, self.HEIGHT)
+
+        self.network = c
+
+        # Initialising pygame, setting up the screen and setting up the clock
+        self.screen = pygame.display.set_mode(self.RESOLUTION)
+        
+    def draw(self):
+        self.screen.fill((0,0,0))
     
-    def __init__(self):
-        self.HEADER = 64  #First message to the server is 64 bytes
-        self.PORT = 5050  #port location
-        self.SERVER = '172.20.53.198' # fill with server ip
-        self.ADDR = (self.SERVER, self.PORT)  #makes a tuple
-        self.FORMAT = 'utf-8'
-        self.DISCONNECT_MESSAGE = "! DISCONNECTED"
+    def update_screen(self):
+        pygame.display.flip()
 
-        self.client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)  # Create socket family/type
-        self.client.connect(self.ADDR)
-
-        self.players = []
-
-        # Objects to send
-        self.player1 = Player(100,100,50,50)
-        self.players.append(self.player1)
-
-    def send_object(self,object):
-       # Serialize the object
-        serialized_data = pickle.dumps(object)
-
-        # Send the serialized object
-        self.client.sendall(serialized_data)
-
-        self.client.close()
-
-#---------------------------------MAIN---------------------------------#
 c = Client()
-c.send_object(c.player1)
+g = Game(c)
+
+while True:
+    #c = Client()
+    
+    g.draw()
+    c.player1.draw_player(g.screen)
+    g.update_screen()
+    #c.send_object(c.player1)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                c.player1.x += 10
+            if event.key == pygame.K_LEFT:
+                c.player1.x -= 10
+            if event.key == pygame.K_DOWN:
+                c.player1.y += 10
+            if event.key == pygame.K_UP:
+                c.player1.y -= 10
+
+        clock.tick(80)
