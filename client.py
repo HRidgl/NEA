5a7fc6a759a55603a12990e964eb3f1655def131
HRidgl <156449683+HRidgl@users.noreply.github.com>
2024-10-04 15:11:44 +0100

    Adding comments

--- client.py
+++ client.py
@@ -8,20 +8,21 @@ class Client:
     def __init__(self):
         self.HEADER = 64  #First message to the server is 64 bytes
         self.PORT = 5050  #port location
-        self.SERVER = '172.20.53.198' # fill with server ip
+        self.SERVER = '172.20.53.198' #server ip
         self.ADDR = (self.SERVER, self.PORT)  #makes a tuple
-        self.FORMAT = 'utf-8'
+        self.FORMAT = 'utf-8' #format to encode and decode data
         self.DISCONNECT_MESSAGE = "! DISCONNECTED"
 
         self.client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)  # Create socket family/type
         self.client.connect(self.ADDR)
 
-        self.players = []
+        self.players = [] #Making an array to store the players
 
-        # Objects to send
+        # Object to send
         self.player1 = Player(100,100,50,50)
         self.players.append(self.player1)
 
+    # Sending the object to the server computer
     def send_object(self,object):
        # Serialize the object
         serialized_data = pickle.dumps(object)
