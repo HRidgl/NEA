4b61fa3698b85daa01ac5f10eef9ac6d52e04b2e
HRidgl <156449683+HRidgl@users.noreply.github.com>
2024-10-04 15:16:28 +0100

    Commenting

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+### This file is used as a central area for all other pages to import from.
+
 #importing all my modules here
 import socket
 import threading
@@ -7,6 +9,8 @@ import sys
 import time
 from random import choice
 
+# Initialising pygame for all files
 pygame.init()
 
+# Setting up the clock
 clock = pygame.time.Clock()
