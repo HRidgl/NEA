5585ae4a5a9a5efa4080128c671f754b396cab20
HRidgl <156449683+HRidgl@users.noreply.github.com>
2024-10-04 15:00:07 +0100

    Update game.py

--- game.py
+++ game.py
@@ -18,16 +18,33 @@ class Game():
     
     def update_screen(self):
         pygame.display.flip()
-        clock.tick(80)
 
 c = Client()
 g = Game(c)
 
 while True:
-    c = Client()
+    
     g.draw()
+    c.player1.draw_player(g.screen)
     g.update_screen()
-    c.send_object(c.player1)
-    c.client.close()
 
-pygame.quit()
+    c.send_object(c.player1.x)
+    c.send_object(c.player1.y)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            c.client.close()
+            pygame.quit()
+            sys.exit()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                c.player1.x += 10
+            if event.key == pygame.K_LEFT:
+                c.player1.x -= 10
+            if event.key == pygame.K_DOWN:
+                c.player1.y += 10
+            if event.key == pygame.K_UP:
+                c.player1.y -= 10
+
+    clock.tick(80)
