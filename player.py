06d9a2cdd37c9d21a2de86cc0b0e29f102e18115
HRidgl <156449683+HRidgl@users.noreply.github.com>
2024-10-04 14:38:15 +0100

    movable player

--- player.py
+++ player.py
@@ -1,3 +1,5 @@
+from main import *
+
 class Player:
     
     def __init__(self,x,y,width,height):
@@ -5,4 +7,14 @@ class Player:
         self.y = y
         self.width = width
         self.height = height
-        self.colour = (0,0,0)
+        self.colour = (255,255,255)
+
+    def draw_player(self,screen):
+        player_square = pygame.Rect(self.x, self.y, 50, 50)
+        pygame.draw.rect(screen, (255, 0, 0), player_square)
+
+    def set_x(self,new_x):
+        self.x = new_x
+
+    def set_y(self,new_y):
+        self.y = new_y
