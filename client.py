0fab5b9d21c5a886497ce9eb010ed848d6e1632d
HRidgl <156449683+HRidgl@users.noreply.github.com>
2024-10-10 10:28:03 +0100

    Update client.py

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
- # Importing all modules from the main
+# Importing all modules from the main
 from main import *
+import socket
+import pickle
 from player import *
 
 # Class used to handle client requests and connects the client to the server
@@ -8,12 +10,13 @@ class Client:
     def __init__(self):
         self.HEADER = 64  #First message to the server is 64 bytes
         self.PORT = 5050  #port location
-        self.SERVER = '172.20.53.198' #server ip
+        self.SERVER = '172.20.52.237' #server ip
         self.ADDR = (self.SERVER, self.PORT)  #makes a tuple
         self.FORMAT = 'utf-8' #format to encode and decode data
         self.DISCONNECT_MESSAGE = "! DISCONNECTED"
 
         self.client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)  # Create socket family/type
+                
         self.client.connect(self.ADDR)
 
         self.players = [] #Making an array to store the players
