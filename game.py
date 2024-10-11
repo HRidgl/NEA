8c3f7c4194c43aa37bd6346a6b22a1390b748d84
HRidgl <156449683+HRidgl@users.noreply.github.com>
2024-10-11 15:15:31 +0100

    Still crashes

--- game.py
+++ game.py
@@ -1,7 +1,5 @@
 # Importing all my modules from main
 from main import *
-from client import *
-from server import *
 
 # Class for my main game
 class Game():
@@ -11,15 +9,16 @@ class Game():
         self.HEIGHT = 600
         self.RESOLUTION = (self.WIDTH, self.HEIGHT)
 
-        # Calling the client class and setting it as an attribute in this class
-        self.network = Client()
-
         # Initialising pygame, setting up the screen and setting up the clock
         self.screen = pygame.display.set_mode(self.RESOLUTION)
 
     # Drawing the background of the screen
     def draw(self):
         self.screen.fill((255,255,255))
+
+    # Updating the screen
+    def update_screen(self):
+        pygame.display.flip()
 
     # Updating the screen
     def update_screen(self):
