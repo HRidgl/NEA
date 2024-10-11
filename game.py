868a85227a44e3552d0767d2a7dd82b5be8f093a
HRidgl <156449683+HRidgl@users.noreply.github.com>
2024-10-11 14:49:46 +0100

    Update game.py

--- game.py
+++ game.py
@@ -1,25 +1,39 @@
 # Importing all my modules from main
 from main import *
 from client import *
+from server import *
 
 # Class for my main game
 class Game():
     
-    def __init__(self,network_type):
+    def __init__(self):
         self.WIDTH = 800
         self.HEIGHT = 600
         self.RESOLUTION = (self.WIDTH, self.HEIGHT)
 
         # Calling the client class and setting it as an attribute in this class
-        self.network = network_type
+        self.network = Client()
 
         # Initialising pygame, setting up the screen and setting up the clock
         self.screen = pygame.display.set_mode(self.RESOLUTION)
 
     # Drawing the background of the screen
     def draw(self):
-        self.screen.fill((0,0,0))
+        self.screen.fill((255,255,255))
 
     # Updating the screen
     def update_screen(self):
         pygame.display.flip()
+
+
+class S_Game(Game):
+    def __init__(self,s):
+        self.WIDTH = 800
+        self.HEIGHT = 600
+        self.RESOLUTION = (self.WIDTH, self.HEIGHT)
+
+        # Calling the client class and setting it as an attribute in this class
+        self.network = s
+
+        # Initialising pygame, setting up the screen and setting up the clock
+        self.screen = pygame.display.set_mode(self.RESOLUTION)
