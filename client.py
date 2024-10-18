3478352c7eb7b45eeb1fbc90a40106742d57102a
HRidgl <156449683+HRidgl@users.noreply.github.com>
2024-10-18 15:19:54 +0100

    Update client.py

--- client.py
+++ client.py
@@ -1,6 +1,8 @@
 # Importing all modules from the main
 from main import *
 from player import *
+import threading
+import sys
 
 # Class used to handle client requests and connects the client to the server
 class Client:
@@ -23,9 +25,8 @@ class Client:
         self.player1 = Player(100,100,50,50)
         self.players.append(self.player1)
 
-    # Sending the object to the server computer
     def send_object(self, obj):
-        # Serialize the object
+
         serialized_data = pickle.dumps(obj)
 
         # Get the length of the serialized data
@@ -36,3 +37,10 @@ class Client:
 
         # Send the header and the serialized object
         self.client.sendall(header + serialized_data)
+
+    def receive_data(self):
+        msg_length = self.SERVER.recv(self.HEADER).decode(self.FORMAT)  # Wait until something is sent over the socket
+        if msg_length:
+            msg_length = int(msg_length)  # Shows length of the message that is about to be recieved
+            msg_length = int(msg_length)  # Shows length of the message that is about to be received
+            msg = self.SERVER.recv(msg_length).decode(self.FORMAT)
