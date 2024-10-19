7e0a3bbe5985bf8ece92b7d29137df8317411abf
HRidgl <156449683+HRidgl@users.noreply.github.com>
2024-10-19 15:52:33 +0100

    improved commenting

--- client.py
+++ client.py
@@ -13,46 +13,28 @@ class Client:
         self.FORMAT = 'utf-8' #format to encode and decode data
         self.DISCONNECT_MESSAGE = "! DISCONNECTED"
 
-        self.client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)  # Create socket family/type
-                
-        self.client.connect(self.ADDR)
+        self.client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)  # Create socket family/type    
+        self.client.connect(self.ADDR) #Connecting the client to the server
 
         self.players = [] #Making an array to store the players
-
-        # Object to send
-        self.player1 = Player(100,100,50,50)
-        self.players.append(self.player1)
+        self.player1 = Player(100,100,50,50) # Creating player object
+        self.players.append(self.player1) # Adding the player to the players array
 
 
-    # Sending the object to the server computer
+    # Method for sending the object to the server computer using pickle sterilisation and sockets
     def send_object(self, obj):
-        # Serialize the object
         serialized_data = pickle.dumps(obj)
-
-        # Get the length of the serialized data
         data_length = len(serialized_data)
-
-        # Create a fixed-size header with the length of the data (64 bytes)
         header = f"{data_length:<{self.HEADER}}".encode(self.FORMAT)
-
-        # Send the header and the serialized object
         self.client.sendall(header + serialized_data)
 
 
+    # Method for receiving objects from the server
     def receive_message(self):
-        try:
-            header = self.client.recv(self.HEADER).decode(self.FORMAT)
-            if header:
-                data_length = int(header.strip())
-                serialized_data = self.client.recv(data_length)
-                data = pickle.loads(serialized_data)
-                print(data)
-            
-        except Exception as e:
-            print(f"Error receiving data: {e}")
-            return None
-
-
-    def disconnect(self):
-        self.send_object(self.DISCONNECT_MESSAGE)
-        self.client.close()
+        header = self.client.recv(self.HEADER).decode(self.FORMAT)
+        if header != '':
+            header = header.strip()
+            data_length = int(header)
+            serialized_data = self.client.recv(data_length)
+            data = pickle.loads(serialized_data)
+            print(data)
